eac20bdfa2e7f6e5118fa273c2db8aa7560d0954
phnaharris <phnaharris@localhost.localdomain>
2022-09-20 22:17:53 +0700

    Install alacritty

--- install.sh
+++ install.sh
@@ -11,6 +11,19 @@ function is_installed {
 }
 
 # Install softwares
+function install_alacritty {
+    sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+    git clone https://github.com/alacritty/alacritty.git
+    cargo build --release
+    cd alacritty
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+    cd .. 
+    rm -r ../alacritty
+}
+
 function install_neovim {
   if [ "(is_installed nvim)" == "0" ]; then
     echo "Neovim not found! Please install neovim first!"
@@ -127,4 +140,4 @@ function backup {
 # --   shift
 # -- done
 # install_programminglanguage
-install_tmux
+install_alacritty
