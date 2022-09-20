6d460e78198128ddfab1d86c6792e0c804f9f753
phnaharris <phnaharris@localhost.localdomain>
2022-09-20 21:25:29 +0700

    Up

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ function install_neovim {
     return
   fi
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  sudo apt install ripgrep -y
+  npm install -g @fsouza/prettierd
 }
 
 function install_dracula {
@@ -26,67 +28,103 @@ function install_dracula {
 }
 
 function install_programminglanguage {
-  if [ "(is_installed ruby)" == "0" ]; then
+  if [ "$(is_installed ruby)" == "0" ]; then
     echo "Installing ruby"
     sudo apt install ruby-full -y
+    sudo gem install jekyll bundler -y
   fi
-  if [ "(is_installed rustup)" == "0" ]; then
+  if [ "$(is_installed rustup)" == "0" ]; then
     echo "Installing rust"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    rustup update
     source $HOME/.cargo/env
+    rustup update
   fi
-  if [ "(is_installed go)" == "0" ]; then
+  if [ "$(is_installed go)" == "0" ]; then
     echo "Installing golang"
     sudo apt install golang -y
+  fi
+  if [ "$(is_installed nvm)" == "0" ]; then # Cannot detect if nvm was installed or not
+    echo "Installing nvm"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    nvm install node
+    npm install -g yarn -y
+    npm install -g gulp -y
   fi
 }
 
 function install_tmux {
-  if [ "(is_installed tmux)" == "0" ]; then
+  if [ "$(is_installed tmux)" == "0" ]; then
     echo "Installing tmux"
     sudo apt install tmux -y
     echo "Installing tpm"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    tmux source ~/.tmux.conf
+    source ~/.tmux.conf
   fi
 }
 
-function install_tools { true; }
-
-function install_zsh { true; }
-
-function backup {
-    mkdir -p .config
-    sudo cp -r ~/.config/alacritty ./.config
-    sudo cp -r ~/.config/nvim ./.config
+function install_tools { 
+  sudo apt install build-essential zlib1g-dev -y
+  sudo apt install aptitude -y
+  sudo apt install snapd
+  sudo snap install telegram-desktop
+  sudo snap install discord
+  sudo snap install skype
+  echo $'\n'"Installing postman"
+  sudo snap install postman
+  echo $'\n'"Installing vscode"
+  sudo apt install code
+  echo $'\n'"Holding vscode"
+  sudo apt-mark hold code
+  # echo $'\n'"Installing latex" # cài sau cùng dùm cái, cài phải hơn 3 tiếng
+  # sudo apt-get install texlive-full -y
 }
 
-while test $# -gt 0; do 
-  case "$1" in
-    --help)
-      echo "Help"
-      exit
-      ;;
-    --macos)
-      install_macos
-      backup
-      link_dotfiles
-      zsh
-      source ~/.zshrc
-      exit
-      ;;
-    --backup)
-      backup
-      exit
-      ;;
-    --dotfiles)
-      link_dotfiles
-      exit
-      ;;
-  esac
+function install_zsh { 
+  local current_date=$(date +%s)
+  sudo apt install zsh -y
+  if [ -d ~/.oh-my-zsh ]; then
+    echo "oh-my-zsh is installed"
+  else
+    echo "oh-my-zsh is not installed"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  fi
+  echo $'OhMyZsh plugin\n'
+  echo $'Downloading zsh-syntax-highlighting zsh-autosuggestions\n'
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
 
-  shift
-done
-Footer
+function backup {
+  mkdir -p .config
+  sudo cp -r ~/.config/alacritty ./.config
+  sudo cp -r ~/.config/nvim ./.config
+}
 
+# -- while test $# -gt 0; do 
+# --   case "$1" in
+# --     --help)
+# --       echo "Help"
+# --       exit
+# --       ;;
+# --     --macos)
+# --       install_macos
+# --       backup
+# --       link_dotfiles
+# --       zsh
+# --       source ~/.zshrc
+# --       exit
+# --       ;;
+# --     --backup)
+# --       backup
+# --       exit
+# --       ;;
+# --     --dotfiles)
+# --       link_dotfiles
+# --       exit
+# --       ;;
+# --   esac
+# -- 
+# --   shift
+# -- done
+# install_programminglanguage
+install_tmux
