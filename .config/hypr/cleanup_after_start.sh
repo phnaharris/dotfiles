22a43b5ac4652bacf39304b87f8c6e040381a7a2
phnaharris <phnanh.harris@gmail.com>
2023-01-07 23:22:01 +0700

    refactor(hyprland): scripts

---
+++ .config/hypr/cleanup_after_start.sh
@@ -0,0 +1,5 @@
+# sleep 10
+# hyprctl keyword windowrule "workspace unset,kitty"
+# hyprctl keyword windowrule "workspace unset,Subl"
+# hyprctl keyword windowrule "workspace unset,Mailspring"
+# hyprctl keyword windowrule "workspace unset,firefox"
