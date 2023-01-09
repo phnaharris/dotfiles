34e6df4bb322efb367072a0dfee4eb0762831b61
phnaharris <phnanh.harris@gmail.com>
2023-01-09 16:51:24 +0700

    refactor(stuff): up

--- .config/nvim/lua/plugins/lsp/settings/clangd.lua
+++ .config/nvim/lua/plugins/lsp/settings/clangd.lua
@@ -2,12 +2,11 @@ local status_util, util = pcall(require, "lspconfig/util")
 if not status_util then return end
 
 return {
-    cmd = { "clangd",
-        "--background-index",
-        "--query-driver=/usr/bin/x86_64-linux-gnu-g++-10",
+    cmd = { "clangd", "--background-index",
+        -- "--query-driver=/usr/bin/x86_64-linux-gnu-g++-10",
         -- "--fallback-style='{BasedOnStyle: llvm, IndentWidth: 8}'",
-    };
-    filetypes = { "c", "cpp", "objc", "objcpp" };
+    },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
     root_dir = util.root_pattern(
         ".clangd",
         ".clang-tidy",
