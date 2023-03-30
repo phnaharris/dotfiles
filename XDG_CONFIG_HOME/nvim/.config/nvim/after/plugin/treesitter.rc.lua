d11e239c117251bf9c2962814ef3fe3ea5673ca7
phnaharris <phnanh.harris@gmail.com>
2023-03-30 09:41:31 +0700

    fix(nvim): treesitter auto install missing

--- XDG_CONFIG_HOME/nvim/.config/nvim/after/plugin/treesitter.rc.lua
+++ XDG_CONFIG_HOME/nvim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -2,6 +2,7 @@ local status_configs, configs = pcall(require, "nvim-treesitter.configs")
 if not status_configs then return end
 
 configs.setup {
+    auto_install = true,
     highlight = {
         enable = true,
         disable = {},
@@ -11,9 +12,9 @@ configs.setup {
         disable = {},
     },
     ensure_installed = { "astro", "c", "cpp", "tsx", "lua", "json", "toml",
-        "yaml", "html", "css", "javascript", "typescript", "norg", "help", "rust",
-        "go", "python", "markdown", "bash", "dockerfile", "julia", "latex",
-        "ruby", "php", "http", "vim", "elixir", "heex", "eex" },
+        "yaml", "html", "css", "javascript", "typescript", "norg", "help",
+        "rust", "go", "python", "markdown", "bash", "dockerfile", "julia",
+        "latex", "ruby", "php", "http", "vim", "elixir", "heex", "eex" },
     autotag = {
         enable = true,
     }
