529228a0389c2f66e42e4bbfb9a0da7145c508b4
phnaharris <phnanh.harris@gmail.com>
2023-10-03 11:50:07 +0700

    fix(nvim): https://github.com/pmizio/typescript-tools.nvim/pull/163

--- XDG_CONFIG_HOME/nvim/.config/nvim/lua/plugins/lsp/settings/tsserver.lua
+++ XDG_CONFIG_HOME/nvim/.config/nvim/lua/plugins/lsp/settings/tsserver.lua
@@ -1,7 +1,3 @@
-local mason_registry = require("mason-registry")
-local tsserver_path = mason_registry.get_package("typescript-language-server")
-    :get_install_path()
-
 return {
     on_attach = require("plugins.lsp.handlers").on_attach,
     capabilities = require("plugins.lsp.handlers").capabilities,
@@ -14,7 +10,5 @@ return {
             "javascriptreact",
             "javascript.jsx",
         },
-        tsserver_path = tsserver_path ..
-            "/node_modules/typescript/lib/tsserver.js",
     }
 }
