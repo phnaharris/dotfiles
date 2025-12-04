0a09d29e3d8ac54ce4c7a25d1bb6978abd469f8b
phnaharris <phnanh.harris@gmail.com>
2025-12-04 23:24:11 +0700

    refactor(fish): clean redundancy

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,53 +4,25 @@ if status --is-interactive
 	end
 end
 
-# Universal Variables
-# A universal variable is a variable whose value is shared across all instances
-# of fish, now and in the future – even after a reboot.
+theme_gruvbox dark hard
 
-setenv EDITOR nvim
-setenv BROWSER brave
-setenv TERMINAL alacritty
-setenv FILEMANAGER thunar
-
-setenv GTK_IM_MODULE fcitx
-setenv QT_IM_MODULE fcitx
-setenv XMODIFIERS @im=fcitx
-setenv QT_QPA_PLATFORM wayland
-setenv MOZ_ENABLE_WAYLAND 1
-
-setenv XDG_CURRENT_DESKTOP sway
-setenv XDG_SESSION_TYPE wayland
-setenv XDG_SESSION_DESKTOP sway
+# setenv GTK_IM_MODULE fcitx
+# setenv QT_IM_MODULE fcitx
+# setenv XMODIFIERS @im=fcitx
+# setenv QT_QPA_PLATFORM wayland
+# setenv MOZ_ENABLE_WAYLAND 1
 
 # Rust specific
 # env = CARGO_TARGET_DIR, $HOME/cargo-target
-setenv CARGO_INCREMENTAL 1
-setenv RUST_BACKTRACE 1
+# setenv CARGO_INCREMENTAL 1
+# setenv RUST_BACKTRACE 1
+setenv EDITOR nvim
 
-setenv ANDROID_HOME $HOME/Android/Sdk
-fish_add_path $ANDROID_HOME/emulator
-fish_add_path $ANDROID_HOME/platform-tools
-
-# Some other variables
 # ignore greeting
 set -g fish_greeting
 
-# Programs
 abbr -a g git
-abbr -a c cargo
-abbr -a o xdg-open
-abbr -a t tmux
-abbr -a tmx tmuxinator
-# Commands
-abbr -a update-grub 'grub-mkconfig -o /boot/grub/grub.cfg'
-abbr -a aurman 'paru -Slq | fzf --multi --preview \'paru -Si {1}\' | xargs -ro paru -S'
-abbr -a sa 'source $(which active-venv)'
-abbr -a up 'sudo pacman -Syu --noconfirm && paru -Sua --noconfirm'
-abbr -a cleanup 'pacman -Qtdq | sudo pacman -Rnsc -'
-abbr -a scheduler 'echo \'notify-send "your message"\' | at 10.20 pm'
-
-abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
+# abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
 
 if command -v eza > /dev/null
 	abbr -a l 'eza -lah'
@@ -68,12 +40,10 @@ end
 [ -x "$(command -v mise)" ]; and eval "$(mise activate)"; or true
 
 # Modifying path
-fish_add_path $HOME
-fish_add_path $HOME/data/repos/dotfiles/scripts/*
+fish_add_path $HOME/scripts
+fish_add_path $HOME/scripts/dev
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/share/nvim/mason/bin
-fish_add_path $HOME/.local/share/solana/install/active_release/bin
-fish_add_path /data/repos/phnaharris/scripts/baricon
 
 # pnpm
 fish_add_path $HOME/.local/share/pnpm
@@ -106,10 +76,3 @@ function fish_prompt
 	set_color green; printf '%s ' (__fish_git_prompt)
 	set_color red; echo -n '$ '
 end
-
-fish_add_path -a /home/phnaharris/.foundry/bin
-
-export PATH="$PATH:/home/phnaharris/.sp1/bin"
-export GPG_TTY=$(tty)
-
-export PATH="$PATH:/home/phnaharris/.soundness/bin"
